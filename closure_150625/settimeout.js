0a66d1fd62e958d81c7594762cfde995098c1382
Your Name <pranshul.sharma_cs23@gla.ac.in>
2025-06-23 12:00:15 +0530

    closure_150625

--- closure_150625/settimeout.js
+++ closure_150625/settimeout.js
@@ -1,3 +1,35 @@
-setTimeout(()=>{
-    console.log("i am pranshul");
-}, 2000);
+// setTimeout(()=>{
+//     console.log("i am pranshul");
+// }, 2000);
+
+// for(var i=0;i<=4; i++){
+//     setTimeout(()=>{
+//         console.log(i);
+//     },2000);
+// }
+
+
+
+// for(let i=0;i<=4; i++){
+//     setTimeout(()=>{
+//         console.log(i);
+//     },2000);
+// }
+
+
+
+// for(const i=0;i<=4;i++){
+//     setTimeout(()=>{
+//         console.log(i);
+//     },2000);
+// }                       // give error because const cannot be reassigned
+
+
+
+// for(let i=0 ; i<=10; i++){
+//     let mul = 1;
+//     mul = 2 * i;
+//     setTimeout(()=>{
+//         console.log(mul);
+//     },5000);
+// }
