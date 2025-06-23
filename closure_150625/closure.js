0eeda85d98883c6aa58019dbfb3dfd8b02fff7f3
Your Name <pranshul.sharma_cs23@gla.ac.in>
2025-06-23 11:26:26 +0530

    closure_150625

--- closure_150625/closure.js
+++ closure_150625/closure.js
@@ -1,1 +1,21 @@
-console.log("hi");
+// CLOSURE
+// function a(){
+//     let grandparent = 10;
+    
+//     function b(){
+//         let parent = 5;
+//         console.log(grandparent);       
+//         function c(){
+//             let child = 2;
+//             console.log(grandparent);
+
+            
+//         }
+//         c();
+        
+//     }
+//     b();
+// }
+// a();
+
+
